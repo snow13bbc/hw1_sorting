af240472d304a3e320131e79be5c836072476209
snow13bbc <snow.naing@ucsf.edu>
2018-01-25 23:29:21 -0800

    Testing edge cases for homework 1

--- test/test_algs.py
+++ test/test_algs.py
@@ -1,5 +1,8 @@
 import numpy as np
 from example import algs
+import random
+import time
+import matplotlib.pyplot as plt
 
 def test_pointless_sort():
     # generate random vector of length 10
@@ -15,20 +18,41 @@ def test_pointless_sort():
     assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
 
 def test_bubblesort():
+
     # Actually test bubblesort here. It might be useful to think about
     # some edge cases for your code, where it might fail. Some things to
     # think about: (1) does your code handle 0-element arrays without
     # failing, (2) does your code handle characters?
 
-    x = np.array([1,2,4,0,1])
-    
+    A = np.array([1,2,4,0,1]) #odd length
+    B = np.array([]) #empty vector
+    C = np.array([2]) #single element vector
+    D = np.array([23, 23, 42, 1, 32, 2]) # Duplicated elements
+    E = np.array([1,2,4,0]) #even length
+
     # for now, just attempt to call the bubblesort function, should
     # actually check output
-    algs.bubblesort(x)
+    assert algs.bubblesort(A) == sorted(A)
+    assert algs.bubblesort(B) == sorted(B)
+    assert algs.bubblesort(C) == sorted(C)
+    assert algs.bubblesort(D) == sorted(D)
+    assert algs.bubblesort(E) == sorted(E)
+
+    # algs.bubblesort(x)
 
 def test_quicksort():
 
-    x = np.array([1,2,4,0,1])
+    A = np.array([1,2,4,0,1]) #odd length
+    B = np.array([]) #empty vector
+    C = np.array([2]) #single element vector
+    D = np.array([23, 23, 42, 1, 32, 2]) # Duplicated elements
+    E = np.array([1,2,4,0]) #even length
     # for now, just attempt to call the quicksort function, should
     # actually check output
-    algs.quicksort(x)
+    assert algs.quicksort(A,0,len(A)-1) == sorted(A)
+    assert algs.quicksort(B,0,len(B)-1) == sorted(B)
+    assert algs.quicksort(C,0,len(C)-1) == sorted(C)
+    assert algs.quicksort(D,0,len(D)-1) == sorted(D)
+    assert algs.quicksort(E,0,len(A)-1) == sorted(E)
+
+    # algs.quicksort(x,0,len(x)-1)
