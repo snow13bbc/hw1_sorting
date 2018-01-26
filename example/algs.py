974e93c7510d32cc9550cb3efece1b0eff88310e
snow13bbc <snow.naing@ucsf.edu>
2018-01-25 23:27:54 -0800

    Bubble and Quick sort Algorithm for Homework 1

--- example/algs.py
+++ example/algs.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+sys.setrecursionlimit(10000)
 
 def pointless_sort(x):
     """
@@ -7,19 +9,27 @@ def pointless_sort(x):
     """
     return np.array([1,2,3])
 
-def bubblesort(x):
-    """
-    Describe how you are sorting `x`
-    """
+def bubblesort(A):
+    n = len(A)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if A[j] > A[j+1] :
+                A[j], A[j+1] = A[j+1], A[j]
+    return A
 
-    assert 1 == 1
-    return x
+def Partition (A,p,r):
+    x = A[r] # choosing the pivot as the last element in the array
+    i = p - 1
+    for j in range(p,r):
+        if A[j] <= x:
+            i = i+1
+            A[i],A[j] = A[j],A[i]
+    A[i+1],A[r]=A[r],A[i+1]
+    return i+1
 
-def quicksort(x):
-    """
-    Describe how you are sorting `x`
-    """
-
-    assert 1 == 1
-    return
-
+def quicksort(A,p,r):
+    if p < r:
+        q = Partition(A,p,r)
+        quicksort(A,p,q-1)
+        quicksort(A,q+1,r)
+        return A
