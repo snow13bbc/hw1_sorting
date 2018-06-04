c6c7a32436384c24282d17dd440af4f9eb0b9b4a
snow13bbc <snow.naing@ucsf.edu>
2018-06-04 14:50:59 -0700

    counting conds and assigns based on feedback

--- example/algs.py
+++ example/algs.py
@@ -2,57 +2,55 @@ import numpy as np
 import sys
 sys.setrecursionlimit(10000)
 
-def pointless_sort(x):
-    """
-    This function always returns the same values to show how testing
-    works, check out the `test/test_alg.py` file to see.
-    """
-    return np.array([1,2,3])
-
 def bubblesort(A):
     n_assign = 0 #number of assignments
     n_cond = 0 #number of conditions
     n = len(A)
+
+    if n == 0:
+        return A, n_assign, n_cond
+
     for i in range(n):
         n_assign += 1
         for j in range(0, n-i-1):
+            n_assign += 1
             if A[j] > A[j+1] :
                 n_cond += 1
                 A[j], A[j+1] = A[j+1], A[j]
                 n_assign +=1
+            else:
+                n_cond += 1
     return A, n_assign, n_cond
 
-def Partition (A,p,r,n_assign,n_cond):
-    n_assign = 0 #number of assignments
-    n_cond = 0 #number of conditions
-    x = A[r] # choosing the pivot as the last element in the array
-    i = p - 1
-    for j in range(p,r):
-        if A[j] <= x:
-            n_cond += 1
-            i = i+1
-            n_assign += 1
-            A[i],A[j] = A[j],A[i]
-            n_assign += 1
-    A[i+1],A[r]=A[r],A[i+1]
-    n_assign += 1
-    return i+1
+qs_assign, qs_cond = 0, 0
+def quicksort(A,p,r):
+    global qs_assign, qs_cond
 
-def quicksort(A,p,r,n_assign,n_cond):
-    n_assign = 0 #number of assignments
-    n_cond = 0 #number of conditions
-
-    if len(A)==0:
-        n_cond += 1
-        return [],n_assign,n_cond
-    if len(A)==1:
+    if len(A) in (0,1):
         n_cond += 1
         return A,n_assign,n_cond
+
+    def partition(arr,first,last):
+        global qs_assign, qs_cond
+
+        x = arr[last] # choosing the pivot as the last element in the array
+        i = first - 1 #start index
+        for j in range(first,last):
+            if A[j] <= x:
+                qs_cond += 1
+                i = i+1
+                qs_assign += 1
+                arr[i],arr[j] = arr[j],arr[i]
+                qs_assign += 2
+        arr[i+1],arr[last]=arr[last],arr[i+1]
+        qs_assign += 2
+        return i+1
+
     if p < r:
-        n_cond += 1
-        q = Partition(A,p,r,n_assign,n_cond)
-        quicksort(A,p,q-1, n_assign,n_cond)
-        n_assign += 1
-        quicksort(A,q+1,r,n_assign,n_cond)
-        n_assign += 1
-        return A, n_assign, n_cond
+        qs_cond += 1
+        q = partition(arr,first,last)
+        quicksort(A,p,q-1)
+        qs_assign += 1
+        quicksort(A,q+1,r)
+        qs_assign += 1
+        return A, qs_assign, qs_cond
