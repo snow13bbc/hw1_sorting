b06093b2814514f99afc18fb2a62b1741f5027e9
snow13bbc <snow.naing@ucsf.edu>
2018-06-04 14:52:43 -0700

    editing assert statements based on feedback

--- test/test_algs.py
+++ test/test_algs.py
@@ -4,19 +4,6 @@ import random
 import time
 import matplotlib.pyplot as plt
 
-def test_pointless_sort():
-    # generate random vector of length 10
-    x = np.random.rand(10)
-
-    # check that pointless_sort always returns [1,2,3]
-    assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
-
-    # generate a new random vector of length 10
-    x = np.random.rand(10)
-
-    # check that pointless_sort still returns [1,2,3]
-    assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
-
 def test_bubblesort():
 
     # Actually test bubblesort here. It might be useful to think about
@@ -30,13 +17,14 @@ def test_bubblesort():
     D = [23, 23, 42, 1, 32, 2] # Duplicated elements
     E = [1,2,4,0] #even length
 
-    # for now, just attempt to call the bubblesort function, should
-    # actually check output
+    # test using exisiting sort function and also, just writing out what
+    # the output should look like manually
+
     assert algs.bubblesort(A)[0] == sorted(A)
     assert algs.bubblesort(B)[0] == sorted(B)
     assert algs.bubblesort(C)[0] == sorted(C)
     assert algs.bubblesort(D)[0] == sorted(D)
-    assert algs.bubblesort(E)[0] == sorted(E)
+    assert algs.bubblesort(E)[0] == [0,1,2,4]
 
     # algs.bubblesort(x)
 
@@ -50,10 +38,10 @@ def test_quicksort():
 
     # for now, just attempt to call the quicksort function, should
     # actually check output
-    assert algs.quicksort(A,0,len(A)-1,0,0)[0] == sorted(A)
-    assert algs.quicksort(B,0,len(B)-1,0,0)[0] == sorted(B)
-    assert algs.quicksort(C,0,len(C)-1,0,0)[0] == sorted(C)
-    assert algs.quicksort(D,0,len(D)-1,0,0)[0] == sorted(D)
-    assert algs.quicksort(E,0,len(E)-1,0,0)[0] == sorted(E)
+    assert algs.quicksort(A,0,len(A)-1)[0] == sorted(A)
+    assert algs.quicksort(B,0,len(B)-1)[0] == sorted(B)
+    assert algs.quicksort(C,0,len(C)-1)[0] == sorted(C)
+    assert algs.quicksort(D,0,len(D)-1)[0] == sorted(D)
+    assert algs.quicksort(E,0,len(E)-1)[0] == [0,1,2,4] 
 
     # algs.quicksort(x,0,len(x)-1)
